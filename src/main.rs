d57c3f58ae0b6f53b13df37e7f0584e5ecea70d7
Yuxuan Shui <yshuiv7@gmail.com>
2020-05-03 03:34:40 +0100

    Print some summary information

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::path::PathBuf;
 mod bcachefs {
 	#![allow(non_upper_case_globals)]
 	#![allow(non_camel_case_types)]
@@ -5,58 +6,108 @@ mod bcachefs {
 	#![allow(unused)]
 
 	include!(concat!(env!("OUT_DIR"), "/bcachefs.rs"));
+
+	impl bch_sb_handle {
+		pub fn get_uuid(&self) -> uuid::Uuid {
+			uuid::Uuid::from_bytes(unsafe { &*self.sb }.user_uuid.b)
+		}
+	}
 }
 
-fn print_sb(sb: &bcachefs::bch_sb_handle) {
-	let user_uuid = unsafe {
-		let sb_inner = &*sb.sb;
-		uuid::Uuid::from_bytes(sb_inner.user_uuid.b)
-	};
-	println!("{}", user_uuid);
+extern "C" {
+	pub static stdout: *mut libc::FILE;
 }
+
+struct FileSystem {
+	/// External UUID of the bcachefs
+	uuid: uuid::Uuid,
+	/// Super block
+	sb: bcachefs::bch_sb_handle,
+	/// Member devices for this filesystem
+	devices: Vec<PathBuf>,
+}
+
+impl FileSystem {
+	fn new(sb: bcachefs::bch_sb_handle) -> Self {
+		Self {
+			uuid: sb.get_uuid(),
+			sb,
+			devices: vec![],
+		}
+	}
+}
+
 fn main() -> anyhow::Result<()> {
+	use std::collections::HashMap;
 	use std::os::unix::ffi::OsStrExt;
 	let mut udev = udev::Enumerator::new()?;
+	let mut fss = HashMap::new();
 	udev.match_subsystem("block")?;
 
-	for dev in udev.scan_devices()? {
-		if let Some(p) = dev.devnode() {
-			let path = std::ffi::CString::new(p.as_os_str().as_bytes()).unwrap();
-			let result = unsafe {
-				let mut opts = std::mem::MaybeUninit::zeroed();
-				let mut sb = std::mem::MaybeUninit::zeroed();
-				let ret = bcachefs::bch2_read_super(
-					path.as_ptr(),
-					opts.as_mut_ptr(),
-					sb.as_mut_ptr(),
-				);
-				if ret == -libc::EACCES {
-					Err(std::io::Error::new(
-						std::io::ErrorKind::PermissionDenied,
-						"no permission",
-					))
-				} else if ret != 0 {
-					Err(std::io::Error::new(
-						std::io::ErrorKind::Other,
-						"Failed to read super",
-					))
-				} else {
-					Ok((opts.assume_init(), sb.assume_init()))
-				}
-			};
-			match result {
-				Ok((_, sb)) => {
-					println!("{} is bcachefs device:", p.display());
-					print_sb(&sb);
-				}
-				Err(e) if e.kind() != std::io::ErrorKind::PermissionDenied => {
-					println!("{} is not a bcachefs device", p.display());
-				}
-				e @ Err(_) => {
-					e?;
+	{
+		// Stop libbcachefs from spamming the output
+		let _gag = gag::Gag::stdout().unwrap();
+		for dev in udev.scan_devices()? {
+			if let Some(p) = dev.devnode() {
+				let path =
+					std::ffi::CString::new(p.as_os_str().as_bytes()).unwrap();
+				let result = unsafe {
+					let mut opts = std::mem::MaybeUninit::zeroed();
+					let mut sb = std::mem::MaybeUninit::zeroed();
+					let ret = bcachefs::bch2_read_super(
+						path.as_ptr(),
+						opts.as_mut_ptr(),
+						sb.as_mut_ptr(),
+					);
+					if ret == -libc::EACCES {
+						Err(std::io::Error::new(
+							std::io::ErrorKind::PermissionDenied,
+							"no permission",
+						))
+					} else if ret != 0 {
+						Err(std::io::Error::new(
+							std::io::ErrorKind::Other,
+							"Failed to read super",
+						))
+					} else {
+						Ok((opts.assume_init(), sb.assume_init()))
+					}
+				};
+				match result {
+					Ok((_, sb)) => match fss.get_mut(&sb.get_uuid()) {
+						None => {
+							let mut fs = FileSystem::new(sb);
+							fs.devices.push(p.to_owned());
+							fss.insert(fs.uuid, fs);
+						}
+						Some(fs) => {
+							fs.devices.push(p.to_owned());
+						}
+					},
+					Err(e) if e.kind()
+						!= std::io::ErrorKind::PermissionDenied =>
+					{
+						()
+					}
+					e @ Err(_) => {
+						e?;
+					}
 				}
 			}
 		}
+		// Flush stdout so buffered output don't get printed after we remove the gag
+		unsafe {
+			libc::fflush(stdout);
+		}
+	}
+
+	println!("Found {} bcachefs filesystems: ", fss.len());
+	for fs in fss.values() {
+		print!("{}: ", fs.uuid);
+		for dev in &fs.devices {
+			print!("{} ", dev.display());
+		}
+		println!("");
 	}
 	Ok(())
 }
